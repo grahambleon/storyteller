7508b97769772edb4126ae7af12b12ba7dad3def
Graham Gilpatric <grahamwithag@gmail.com>
2018-08-26 21:21:38 -0400

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
-// import fetch from 'isomorphic-fetch'
 
 configure({ adapter: new Adapter() });
 
